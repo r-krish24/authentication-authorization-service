f96560a9c210b7255b71716b2dc8325848aa7050
Ram Krishnan <ramkrishnane@maveric-systems.com>
2022-09-21 15:28:22 +0530

    Added logs

--- src/main/java/com/maveric/authenticationauthorizationservice/constants/Type.java
+++
@@ -1,5 +0,0 @@
-package com.maveric.authenticationauthorizationservice.constants;
-
-public enum Type {
-    MALE,FEMALE;
-}
--- src/main/java/com/maveric/authenticationauthorizationservice/controller/AuthController.java
+++ src/main/java/com/maveric/authenticationauthorizationservice/controller/AuthController.java
@@ -7,6 +7,7 @@ import com.maveric.authenticationauthorizationservice.feignconsumer.UserServiceC
 import com.maveric.authenticationauthorizationservice.model.UserPrincipal;
 import com.maveric.authenticationauthorizationservice.service.UserService;
 import com.maveric.authenticationauthorizationservice.util.JwtUtil;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,8 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/v1")
 public class AuthController {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(AuthController.class);
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -36,12 +39,14 @@ public class AuthController {
     /* Checks user credentials for login and returns JWT token along with user information*/
     @PostMapping("auth/login")
     public ResponseEntity<AuthResponseDto> authLogin(@Valid @RequestBody AuthRequestDto authRequestDto) {
+        log.info("API call for login validation");
         try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequestDto.getEmail(), authRequestDto.getPassword())
             );
         }
         catch (BadCredentialsException e) {
+            log.error("Incorrect username or password");
             throw new InvalidCredentialsException("Incorrect username or password");
         }
         final UserPrincipal userPrincipal = (UserPrincipal)userService
@@ -51,12 +56,14 @@ public class AuthController {
         AuthResponseDto authResponseDto = new AuthResponseDto();
         authResponseDto.setToken(jwt);
         authResponseDto.setUser(userPrincipal.getUser());
+        log.info("Login successful, JWT token returned.");
         return new ResponseEntity<>(authResponseDto, HttpStatus.OK);
     }
 
     /* Creates user for login and returns JWT token along with user information*/
     @PostMapping("auth/signup")
     public ResponseEntity<AuthResponseDto> authSignUp(@Valid @RequestBody UserDetailsDto userDetailsDto) {
+        log.info("API call for signup");
         ResponseEntity<UserDetailsDto> userDetailsDtoRespEntity = userServiceConsumer.createUser(userDetailsDto);
         UserDetailsDto userDetailsDtoResp = userDetailsDtoRespEntity.getBody();
         AuthResponseDto authResponseDto = new AuthResponseDto();
@@ -67,8 +74,10 @@ public class AuthController {
             authResponseDto.setUser(userPrincipal.getUser());
         }
         else {
+            log.error("Account creation failed, Check User service!");
             throw new AccountCreationFailedException("Account creation failed");
         }
+        log.info("Signup successful, JWT token returned.");
         return new ResponseEntity<>(authResponseDto, HttpStatus.CREATED);
     }
 
@@ -81,8 +90,8 @@ public class AuthController {
 
     /* End point to test authentication through JWT token  */
     @GetMapping("/hello")
-    public String sampleAPI() {
-        return "Hello Maveric!";
+    public String sampleAPI(@RequestHeader String userEmail,@RequestHeader String userId) {
+        return "Hello "+userEmail+"---"+userId+", \nWelcome to Maveric Bank!";
     }
 
 
